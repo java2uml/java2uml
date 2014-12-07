6f8ca6dc159939f4f778a087f55225db05dd2c8f
Vladimir Malov <vldmalov@gmail.com>
2014-12-07 23:43:40 +0300

    add test_data folder with simple classes for testing project

---
+++ test_data/src/com/abalyschev/java2/lesson7/task1/Engine.java
@@ -0,0 +1,23 @@
+package com.abalyschev.java2.lesson7.task1;
+
+public class Engine {
+    private int power;
+
+    public Engine() {
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    @Override
+    public String toString() {
+        return "Engine{" +
+                "power=" + power +
+                '}';
+    }
+}
---
+++ test_data/src/com/abalyschev/java2/lesson7/task1/Gear.java
@@ -0,0 +1,6 @@
+package com.abalyschev.java2.lesson7.task1;
+
+public interface Gear {
+    void setGear(int gear);
+    void nextGear();
+}
