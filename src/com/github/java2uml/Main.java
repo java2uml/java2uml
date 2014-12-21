5f09e1b01a9badb383c233fe6640f4068e3c4b9c
Igaleksus <igaleksus@yandex.ru>
2014-12-21 23:33:52 +0300

    no message

--- src/com/github/java2uml/Main.java
+++ src/com/github/java2uml/Main.java
@@ -1,5 +1,11 @@
 package com.github.java2uml;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -7,31 +13,93 @@ import java.util.Set;
 
 public class Main {
     static String[] args;
+    UI ui;
+
+    //Из класса UI эту переменную меняет JFileChooser, при выборе директории
+    public static String path;
+
+    public static String getPath() {
+        return path;
+    }
+
+    public static void setPath(String _path) {
+        path = _path;
+    }
 
     public static void main(String[] args) throws Exception {
         System.out.println("Java2UML starting point...");
         Main.args = args;
-
-        new Main().go();
+        Main main = new Main();
+        main.go();
     }
 
     private void go() throws Exception {
         Method[] methods;
         Field[] fields;
         Constructor[] constructors;
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                initUI();
+            }
+        });
 
 
-        String path = args[0];
 
+
+    }
+
+    public void initUI (){
+        ui = new UI();
+        ui.initUI().setVisible(true);
+        ui.addActionListenerToChooseFile();
+        ui.getGeneratePlantUML().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Thread generateDiagramThread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        loadClassesAndGenerateDiagram(ui.getPath().getText());
+                    }
+                });
+                generateDiagramThread.start();
+
+            }
+        });
+    }
+
+    public void loadClassesAndGenerateDiagram(String path){
         UMLClassLoader ecl = new UMLClassLoader();
+        Set<Class> classes = null;
 
-        Set<Class> classes = ecl.loadClasses(path);
+        ui.increaseProgressBarForTwenty();
+
+        try {
+             classes = ecl.loadClasses(path);
+        } catch (ClassNotFoundException ex){
+            ex.printStackTrace();
+        }
+
+        ui.increaseProgressBarForTwenty();
 
         //todo убрать вывод в консоль после тестирования
         System.out.println("Классы загружены, передаем на обработку.");
         System.out.println("----------------------------------------");
 
+
+
+        for( Class clazz : classes ) {
+            System.out.println(clazz.getName());
+            ui.increaseProgressBarForTwenty();
+        }
+
         String diagram = DataExtractor.extract(classes);
+//        System.out.println(diagram);
+        ui.getGeneratedCode().setText(diagram);
         DataExtractor.generate(diagram);
+
+        ui.setProgressBarComplete();
+
+        ui.showDiagram();
     }
 }
