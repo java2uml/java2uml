83b6d0b29fc85b1e3fb5b71f6b704dceb72a83a8
Igor <igaleksus@yandex.ru>
2014-12-30 15:19:53 +0300

    deleted JavaObjectCompose

--- src/com/github/java2uml/gui/UI.java
+++ src/com/github/java2uml/gui/UI.java
@@ -154,15 +154,15 @@ public class UI {
         helpItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
-                if (!Help.helpIsNull()) {
-                    if (!helpWindow.isVisible()) {
-                        helpWindow.setVisible(true);
-                    } else {
-                        helpWindow.toFront();
-                        helpWindow.repaint();
-                    }
-                } else helpWindow = Help.getInstance();
+            new Help("Help");
+//                if (!Help.helpIsNull()) {
+//                    if (!helpWindow.isVisible()) {
+//                        helpWindow.setVisible(true);
+//                    } else {
+//                        helpWindow.toFront();
+//                        helpWindow.repaint();
+//                    }
+//                } else helpWindow = Help.getInstance();
             }
         });
 
